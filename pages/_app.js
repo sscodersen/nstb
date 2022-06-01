b3c5514275d54eb5ff14453564cdc1804c1266af
Your Name <you@example.com>
2022-06-01 16:43:26 -0700

    updatedanalcde

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
 
 function MyApp({ Component, pageProps }) {
-  ReactGA.initialize('UA-159961279-2');
   return (
     <>
       <span className="theme-bejamas" />
