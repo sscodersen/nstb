2f43d7b9cb812b6eb21662f68bc2a472713c04ea
Your Name <you@example.com>
2022-07-02 13:59:59 -0700

    gtagrep

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,10 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Script
-    strategy="lazyOnload"
-    src={`https://www.googletagmanager.com/gtag/js?id=G-N5H2ZVHKEG`}
-    />
-
-
-    <Script strategy="lazyOnload">
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-N5H2ZVHKEG');
-      `}
-    </Script>
-
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
