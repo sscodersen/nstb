fcc176000357495d26b59674f56d4645bb3cc77a
Your Name <you@example.com>
2022-07-02 13:28:09 -0700

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,22 @@ import 'prismjs/themes/prism-tomorrow.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <Script
+    strategy="lazyOnload"
+    src={`https://www.googletagmanager.com/gtag/js?id=G-N5H2ZVHKEG`}
+    />
+
+
+    <Script> strategy="lazyOnload">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-N5H2ZVHKEG');
+      `}
+    </Script>
+
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
