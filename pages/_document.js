d4bf1736b7a7315a76b860471b582d1958b9f985
Your Name <you@example.com>
2022-07-07 04:10:44 -0700

    googleadanal

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { useAmp } from 'next/amp'
+
+import { GA_TRACKING_ID } from '../lib/gtag'
+import AmpAnalytics from '../components/amp/AmpAnalytics'
+
+function AmpWrap({ ampOnly, nonAmp }) {
+  const isAmp = useAmp()
+  if (ampOnly) return isAmp && ampOnly
+  return !isAmp && nonAmp
+}
 
 class MyDocument extends Document {
   render() {
@@ -11,9 +21,57 @@ class MyDocument extends Document {
         >
           <Main />
           <NextScript />
+
+          {/* AMP - Google Analytics */}
+          <AmpWrap
+            ampOnly={
+              <AmpAnalytics
+                type="googleanalytics"
+                script={{
+                  vars: {
+                    account: GA_TRACKING_ID,
+                    gtag_id: GA_TRACKING_ID,
+                    config: {
+                      [GA_TRACKING_ID]: { groups: 'default' },
+                    },
+                  },
+                  triggers: {
+                    trackPageview: {
+                      on: 'visible',
+                      request: 'pageview',
+                    },
+                  },
+                }}
+              />
+            }
+          />
+
+          {/* Non-AMP - Google Analytics */}
+          <AmpWrap
+            nonAmp={
+              <>
+              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6096745235213194"></script>
+                <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                />
+                <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', '${GA_TRACKING_ID}');
+                    `,
+                  }}
+                />
+              </>
+            }
+          />
         </body>
       </Html>
-    );
+    )
   }
 }
 
