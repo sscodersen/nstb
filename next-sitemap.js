9699d20e09e0d0ad2328c6a4bacbfa3ce9191c59
Your Name <you@example.com>
2022-07-07 07:27:09 -0700

    SEO

---
+++ next-sitemap.js
@@ -0,0 +1,17 @@
+const siteUrl = "https://blog.socialtribe.xyz/";
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", disallow: "/secret" },
+      { userAgent: "*", allow: "/" },
+    ],
+    additionalSitemaps: [
+      `${siteUrl}/sitemap.xml`,
+      `${siteUrl}/server-sitemap.xml`,
+    ],
+  },
+  exclude: ["/secret"],
+};
