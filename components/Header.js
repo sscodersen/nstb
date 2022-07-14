9f49cc0457d38f472cbc9c45b223f6a10195076b
Your Name <you@example.com>
2022-07-14 12:46:57 -0700

    Amazonecpm

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,7 @@ export default function Header({ name }) {
           <a>{name}</a>
         </Link>
       </p>
+      <div id="amzn-assoc-ad-2b9156a5-bc0a-4362-8dd0-864b2cf64bd6"></div><script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=2b9156a5-bc0a-4362-8dd0-864b2cf64bd6"></script>
     </header>
   );
 }
