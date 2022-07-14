f5783d291fba9345b3b37d3b084db9dec709bf9e
Your Name <you@example.com>
2022-07-13 20:49:06 -0700

    ProPush

--- components/SEO.js
+++ components/SEO.js
@@ -7,6 +7,21 @@ export default function SEO({ title, description }) {
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta name="pushsdk" content="0fe612c4ddf57942bb226eecaf09176a"></meta>
+      <script>
+    var s = document.createElement('script');
+    s.src='//redrotou.net/pfe/current/micro.tag.min.js?z=5239314'+'&sw=/sw-check-permissions-f3a9a.js';
+    s.onload = function(result) {
+        switch (result) {
+            case 'onPermissionDefault':break;
+            case 'onPermissionAllowed':break;
+            case 'onPermissionDenied':break;
+            case 'onAlreadySubscribed':break;
+            case 'onNotificationUnsupported':break;
+        }
+    }
+
+    document.head.appendChild(s);
+</script>
     </Head>
   );
 }
