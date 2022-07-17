aebfca1aa4ee19687da3942368da593a53e1d1f6
Your Name <you@example.com>
2022-07-16 20:11:59 -0700

    Adshares

--- pages/_document.js
+++ pages/_document.js
@@ -51,9 +51,9 @@ class MyDocument extends Document {
           <AmpWrap
             nonAmp={
               <>
-              <div class="_0cbf1c3d417e250a" data-zone="f41e39e66579408eba7b855b3843b499" style="width:320px;height:100px;display: inline-block;margin: 0 auto"></div>
-              <div class="_0cbf1c3d417e250a" data-zone="f41e39e66579408eba7b855b3843b499" style="width:320px;height:100px;display: inline-block;margin: 0 auto"></div>
-              <div class="_0cbf1c3d417e250a" data-options="count=3,interval=1,burst=4" data-zone="2250bc54f5444def8812d19fbee45522" style="display: none"></div>
+<div className="_0cbf1c3d417e250a" data-options="count=3,interval=1,burst=4" data-zone="0c68e4d052934c34bfd428e0db2534a1" style="display: none"></div>
+<div className="_0cbf1c3d417e250a" data-options="count=3,interval=1,burst=4" data-zone="2250bc54f5444def8812d19fbee45522" style="display: none"></div>
+<div className="_0cbf1c3d417e250a" data-zone="f41e39e66579408eba7b855b3843b499" style="width:320px;height:100px;display: inline-block;margin: 0 auto"></div>
               <script type="text/javascript" src="https://app.web3ads.net/main.js" async></script>
               <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6096745235213194"></script>
                 <script
